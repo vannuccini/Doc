11b8fd65ba27c2fb88d963c851d39d22fcf2bda6
Vannuccini Federico <49145826+vannuccini@users.noreply.github.com>
2019-12-01 16:53:24 +0100

    Add files via upload

---
+++ PAIRED_extract_fastq.sh
@@ -0,0 +1,7 @@
+
+#! /bin/sh
+for item in $(cat paired_SRAs.txt);
+do
+  echo $item
+  ./fasterq-dump --split-3 $item -e 10 -O /SPICE/federico_vannuccini/DATA/ALL_SRAs -p -t /SPICE/federico_vannuccini/DATA/tmp && cd && cd ncbi/public/sra  && rm *.cache || rm *.sra || ls && cd && cd / && cd SPICE/federico_vannuccini/DATA/sratoolkit.2.10.0-ubuntu64/bin
+done
